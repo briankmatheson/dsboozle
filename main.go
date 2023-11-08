21f2d6e3decc79238c1114fe39192f50789e473f
Brian Matheson <bmath@brianmatheson.com>
2023-11-08 05:33:39 -0500

    crud

--- main.go
+++ main.go
@@ -14,6 +14,24 @@ type FileType struct {
 	Lossless bool
 }
 
+func CreateFileType(db *gorm.DB, filetype *FileType) error {
+	result := db.Create(&filetype)
+	return result.Error
+}
+func ReadFileType(db *gorm.DB, id uint) (FileType, error) {
+	var filetype FileType
+	result := db.First(&filetype, id)
+	return filetype, result.Error
+}
+func UpdateFileType(db *gorm.DB, filetype *FileType) error {
+	result := db.Save(&filetype)
+	return result.Error
+}
+func DeleteFileType(db *gorm.DB, id uint) error {
+	result := db.Delete(&FileType{}, id)
+	return result.Error
+}
+
 type TrackFile struct {
 	gorm.Model
 	Url        string
@@ -23,9 +41,45 @@ type TrackFile struct {
 	Checksum   string
 }
 
+func CreateTrackFile(db *gorm.DB, trackfile *TrackFile) error {
+	result := db.Create(&trackfile)
+	return result.Error
+}
+func ReadTrackFile(db *gorm.DB, id uint) (TrackFile, error) {
+	var trackfile TrackFile
+	result := db.First(&trackfile, id)
+	return trackfile, result.Error
+}
+func UpdateTrackFile(db *gorm.DB, trackfile *TrackFile) error {
+	result := db.Save(&trackfile)
+	return result.Error
+}
+func DeleteTrackFile(db *gorm.DB, id uint) error {
+	result := db.Delete(&TrackFile{}, id)
+	return result.Error
+}
+
 type Bank struct {
 	gorm.Model
 	Name string
+}
+
+func CreateBank(db *gorm.DB, bank *Bank) error {
+	result := db.Create(&bank)
+	return result.Error
+}
+func ReadBank(db *gorm.DB, id uint) (Bank, error) {
+	var bank Bank
+	result := db.First(&bank, id)
+	return bank, result.Error
+}
+func UpdateBank(db *gorm.DB, bank *Bank) error {
+	result := db.Save(&bank)
+	return result.Error
+}
+func DeleteBank(db *gorm.DB, id uint) error {
+	result := db.Delete(&Bank{}, id)
+	return result.Error
 }
 
 type User struct {
@@ -33,6 +87,24 @@ type User struct {
 	Email string
 	BuyID int
 	Buy   Bank `gorm:"foreignKey:BuyID"`
+}
+
+func CreateUser(db *gorm.DB, user *User) error {
+	result := db.Create(&user)
+	return result.Error
+}
+func ReadUser(db *gorm.DB, id uint) (User, error) {
+	var user User
+	result := db.First(&user, id)
+	return user, result.Error
+}
+func UpdateUser(db *gorm.DB, user *User) error {
+	result := db.Save(&user)
+	return result.Error
+}
+func DeleteUser(db *gorm.DB, id uint) error {
+	result := db.Delete(&User{}, id)
+	return result.Error
 }
 
 type Artist struct {
@@ -43,11 +115,47 @@ type Artist struct {
 	Sell   Bank `"gorm:foreignKey:SellID"`
 }
 
+func CreateArtist(db *gorm.DB, artist *Artist) error {
+	result := db.Create(&artist)
+	return result.Error
+}
+func ReadArtist(db *gorm.DB, id uint) (Artist, error) {
+	var artist Artist
+	result := db.First(&artist, id)
+	return artist, result.Error
+}
+func UpdateArtist(db *gorm.DB, artist *Artist) error {
+	result := db.Save(&artist)
+	return result.Error
+}
+func DeleteArtist(db *gorm.DB, id uint) error {
+	result := db.Delete(&Artist{}, id)
+	return result.Error
+}
+
 type Contract struct {
 	gorm.Model
 	Parties      []User `gorm:"many2many:contract_parties;"`
 	MasterFileID int
-	MasterFile   TrackFile `"gorm:foreignKey:MasterFileIDf"`
+	MasterFile   TrackFile `"gorm:foreignKey:MasterFileID"`
+}
+
+func CreateContract(db *gorm.DB, contract *Contract) error {
+	result := db.Create(&contract)
+	return result.Error
+}
+func ReadContract(db *gorm.DB, id uint) (Contract, error) {
+	var contract Contract
+	result := db.First(&contract, id)
+	return contract, result.Error
+}
+func UpdateContract(db *gorm.DB, contract *Contract) error {
+	result := db.Save(&contract)
+	return result.Error
+}
+func DeleteContract(db *gorm.DB, id uint) error {
+	result := db.Delete(&Contract{}, id)
+	return result.Error
 }
 
 type Distribution struct {
@@ -58,8 +166,44 @@ type Distribution struct {
 	Status    Upload `"gorm:foreignKey:UploadID"`
 }
 
+func CreateDistribution(db *gorm.DB, distribution *Distribution) error {
+	result := db.Create(&distribution)
+	return result.Error
+}
+func ReadDistribution(db *gorm.DB, id uint) (Distribution, error) {
+	var distribution Distribution
+	result := db.First(&distribution, id)
+	return distribution, result.Error
+}
+func UpdateDistribution(db *gorm.DB, distribution *Distribution) error {
+	result := db.Save(&distribution)
+	return result.Error
+}
+func DeleteDistribution(db *gorm.DB, id uint) error {
+	result := db.Delete(&Distribution{}, id)
+	return result.Error
+}
+
 type Upload struct {
 	PercentComplete int
+}
+
+func CreateUpload(db *gorm.DB, upload *Upload) error {
+	result := db.Create(&upload)
+	return result.Error
+}
+func ReadUpload(db *gorm.DB, id uint) (Upload, error) {
+	var upload Upload
+	result := db.First(&upload, id)
+	return upload, result.Error
+}
+func UpdateUpload(db *gorm.DB, upload *Upload) error {
+	result := db.Save(&upload)
+	return result.Error
+}
+func DeleteUpload(db *gorm.DB, id uint) error {
+	result := db.Delete(&Upload{}, id)
+	return result.Error
 }
 
 type Track struct {
@@ -82,6 +226,24 @@ type Track struct {
 	Comment      string
 }
 
+func CreateTrack(db *gorm.DB, track *Track) error {
+	result := db.Create(&track)
+	return result.Error
+}
+func ReadTrack(db *gorm.DB, id uint) (Track, error) {
+	var track Track
+	result := db.First(&track, id)
+	return track, result.Error
+}
+func UpdateTrack(db *gorm.DB, track *Track) error {
+	result := db.Save(&track)
+	return result.Error
+}
+func DeleteTrack(db *gorm.DB, id uint) error {
+	result := db.Delete(&Track{}, id)
+	return result.Error
+}
+
 type Collection struct {
 	gorm.Model
 	Name    string
@@ -95,31 +257,10 @@ type Collection struct {
 
 type Entitlement struct {
 	gorm.Model
-	TrackID  int
-	Track    Track `gorm:"foreignkey=TrackID"`
-	Consumer User
-	Owner    Artist
-}
-
-func CreateTrack(db *gorm.DB, track *Track) error {
-	result := db.Create(&track)
-	return result.Error
-}
-
-func ReadTrack(db *gorm.DB, id uint) (Track, error) {
-	var track Track
-	result := db.First(&track, id)
-	return track, result.Error
-}
-
-func UpdateTrack(db *gorm.DB, track *Track) error {
-	result := db.Save(&track)
-	return result.Error
-}
-
-func DeleteTrack(db *gorm.DB, id uint) error {
-	result := db.Delete(&Track{}, id)
-	return result.Error
+	CollectionID int
+	Collection   Collection `gorm:"foreignKey=CollectionID"`
+	Consumer     User
+	Owner        Artist
 }
 
 func main() {
